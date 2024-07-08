5ca4177077b18247a0951c792948117236269c31
Luke <info@2009_coda>
2024-07-08 10:44:48 +0300

    create model admin on main

--- main/models.py
+++ main/models.py
@@ -98,3 +98,14 @@ class GalleryImage(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Donation(models.Model):
+    donor_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.donor_name} - {self.amount}"        
