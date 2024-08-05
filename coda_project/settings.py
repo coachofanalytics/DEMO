6a93e0f38b1fc434c9bb5c7270e28250afb658aa
hashimkha1 <khah7565@gmail.com>
2024-08-05 17:56:57 +0500

    changes

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "!cxl7yhjsl00964n=#e-=xblp4u!hbajo2k8u#$v9&s6__5=xf"
 
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['biasharabridges.org', 'www.biasharabridges.org', 'biasharadev-68034baaf749.herokuapp.com']
+
 AUTH_USER_MODEL = "accounts.CustomerUser"
 AUTHENTICATION_BACKENDS = (("accounts.custom_backend.EmailOrUsernameModelBackend"), ("django.contrib.auth.backends.ModelBackend"))
 
