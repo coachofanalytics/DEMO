9c0d939733844939f1fc7e1483fa1cf140b7170d
luke <luke@gmail.com>
2024-08-22 21:50:37 -0700

    create a models admin in main  app

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Assets,Feedback,Description,Page,Team,Content,Service,SubService,News,Training,Pricing
+from .models import Assets,Feedback,Description,Page,Team,Content,Service,SubService,News,Training,Pricing,TeamMember
 
 # Register your models here.
 admin.site.register(Assets)
@@ -14,5 +14,6 @@ admin.site.register(SubService)
 admin.site.register(News)
 admin.site.register(Training)
 admin.site.register(Pricing)
+admin.site.register(TeamMember)
 
 
