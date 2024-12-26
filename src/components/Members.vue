3223b4b9fffd33f3b332734417f58bf81899f3e0
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2024-12-26 00:46:56 -0600

    add data to About US on Team

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,29 +1,38 @@
 <template>
-    <div class="about">
       <h1>Our Team</h1>
       <p>{{ description }}</p>
-    </div>
-    <div v-for = "member in members" :key="member" class="TeamMember">
-      <span @click="deleteMember(member)">{{member}}</span>
+    <div v-for = "member in members" :key="member.id" class="TeamMember">
+      <!-- <span @click="deleteMember(member)">{{member}}</span> -->
+      <!-- <router-link :to="{ name: 'MemberDetails', params: { id: member.id }}">
+        <h2>{{ member.id }}</h2>
+        <h2>{{ member.name }}</h2>
+        <h2>{{ member.title }}</h2>
+      </router-link> -->
+      <h2>{{ member.id }}</h2>
+      <h2>{{ member.name }}</h2>
+      <h2>{{ member.title }}</h2>
     </div>
   </template>
-  
+
+
 <script>
 export default {
   name: 'Members',
   props:['header','description','theme'],
-  
+
   data() {
-    return{
-        first_name:'',
-        last_name:'',
-        tenure:0,
+      return {
+        members: [
+          {name:'Chris Maghas', title: 'Project Manager', id: 1, details: 'lorem',tenure:0},
+          {name:'Nane Doe', title: 'UX Designer', id: 2, details: 'lorem',tenure:0 },
+          {name:'Sera Angie',title:'Web Developer', id: 3, details: 'lorem',tenure:0 },
+          {name:'Luke Bonnie', title: 'Vue Developer', id: 4, details: 'lorem',tenure:0 }
+        ],
         description:"The Board Members of CODA comprise a distinguished group of leaders with a broad spectrum of expertise in the IT industry and business management.",
-        members:['Chris','James','Nick'],
-        
-      }
-  },
-  methods : {
+      };
+    },
+
+    methods : {
     deleteMember() {
     //   this.members = this.members.filter((item) => {
     //   return members !== item;
