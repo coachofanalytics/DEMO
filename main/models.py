702740771ed6901cf991c5deeb9140c777126544
luke <luke@gmail.com>
2025-01-04 11:16:39 -0800

    create model admin on main app

--- main/models.py
+++ main/models.py
@@ -77,3 +77,15 @@ class Feedback(models.Model):
     def __str__(self):
         return self.title
     
+
+from django.db import models
+
+class GallerysImage(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='gallery/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    event_date = models.DateField()
+
+    def __str__(self):
+        return self.title
